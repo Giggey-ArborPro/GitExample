115b7a1812c8559cb263df97da5a9a353ca05ee0
Andrew Giggey <agiggey@arborprousa.com>
2026-04-28 12:29:20 -0500

    A different comment
    Signed-off-by: Andrew Giggey <agiggey@arborprousa.com>

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // git config --global alias.adog "log --all --decorate --oneline --graph"
 
+// Goodbyte
 Console.WriteLine("Hello, World!!");
 Console.WriteLine("Heloo from line 2");
 Console.WriteLine("Heloo from line 3");
