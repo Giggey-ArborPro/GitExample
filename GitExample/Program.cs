bfa827b362b13612e1310e4349f8d2ff308fca3d
Andrew Giggey <agiggey@arborprousa.com>
2026-04-28 12:19:07 -0500

    Add Line5
    Signed-off-by: Andrew Giggey <agiggey@arborprousa.com>

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -4,3 +4,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Heloo from line 2");
 Console.WriteLine("Heloo from line 3");
+Console.WriteLine("Heloo from line 4");
+Console.WriteLine("Heloo from line 5");
