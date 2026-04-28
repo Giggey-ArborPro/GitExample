3245b2b69a1793b552ce09299a9940239764ab1f
Andrew Giggey <agiggey@arborprousa.com>
2026-04-28 12:32:16 -0500

    A great comment was added!: A conflict!
    Signed-off-by: Andrew Giggey <agiggey@arborprousa.com>

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // git config --global alias.adog "log --all --decorate --oneline --graph"
 
-// Goodbyte
+
+// HI
+
 Console.WriteLine("Hello, World!!");
